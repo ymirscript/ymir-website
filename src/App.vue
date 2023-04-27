09a51b39faa43b28161959eff58cb554ce2c60a0
Hufnagel <lukas.hufnagel@MB0022.local>
2023-04-27 16:17:35 +0200

    adding project structure and home container

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav class="site-nav">
+      <a class="navitem" href="#home">
+        Home
+      </a>
+      <a class="navitem">
+        Features
+      </a>
+      <a class="navitem">
+        Installation
+      </a>
+      <a class="navitem">
+        More
+      </a>
+    </nav>
+
+    <div class="top-container" id="home">
+      <div class="introduction">
+        <h2><span class="highlight">Ymir</span>: Unleash the power of adaptive <span class="highlight">code generation</span> for <span class="highlight">modern web development</span></h2>
+        <p>Simplify more. Adapt faster. Expand easier.</p>
+        <small>With an intuitive definition language, YmirScript, effortlessly create and manage REST APIs for any target language. Ymir's modular architecture allows seamless integration of target plugins for added flexibility. Experience the power of Code Adaptation, updating boilerplate code without affecting production code, preserving project progress. Harness the efficiency of Ymir, the next generation adaptive compiler for web developers.</small>
+      </div>
+      <div class="logo">
+        <img src="/ymiricon.png"/>
+      </div>
+    </div>
+  </div>
+</template>
